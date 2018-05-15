1e8305bd73f0f56cc89bae553d6c29579eb3bc26
Paul Delarue <Paul Delarue>
2018-05-15 16:22:59 +0200

    fonction de réflexion et de réfraction de la lumière

--- module c/fonctionsDéviationRayon.c
+++ module c/fonctionsDéviationRayon.c
@@ -18,7 +18,7 @@
 		sPos *peak;
 		sPlanEqua planEqua;
 		double refractiveIndex;
-		int reflection;   // 0 si opaque, 1 si réfléchi
+		int reflection;   // 0 si opaque, 1 si réfléchissant
 	};
 
 */
@@ -92,15 +92,60 @@ sParamEqua reflectedRay (sParamEqua incidentRay, sFace face){
 	return reflectedRay;
 }
 
+double* calcAngleWithSnellDescartes (double teta[2], sPos orientationVectorIncidentRay, sPos normalisedVector, double refractiveIndexA, double refractiveIndexB){
+	double scalarProduct = 0;
 
-sParamEqua refractedRay(sParamEqua incidentRay, sFace face){
+//calcule de teta 1
+	//produit scalaire
+	scalarProduct = normalisedVector.x*orientationVectorIncidentRay.x + normalisedVector.y*orientationVectorIncidentRay.y + normalisedVector.z*orientationVectorIncidentRay.z;
+	//formule calcule d'angle à partir de la formule du produit scalaire avec les normes et l'angle
+	teta[0] = acos(scalarProduct / sqrt( ( pow(normalisedVector.x,2) + pow(normalisedVector.y,2) + pow(normalisedVector.z,2) )*( pow(orientationVectorIncidentRay.x,2) + pow(orientationVectorIncidentRay.y,2) + pow(orientationVectorIncidentRay.z,2) ) ) );
+//calcule de teta 2
+	//Formule Snell-Descartes
+	teta[1] = asin((refractiveIndexA/refractiveIndexB)*sin(teta[0]));
+
+	return teta
+}
+
+
+sParamEqua refractedRay(sParamEqua incidentRay, sFace face, double refractiveIndexA, double refractiveIndexB){
+	sPos pI;
 	sPos normalisedVector; //vecteur normal "n"au plan (pointant vers l'exterieur)
+	sPos orientationVectorIncidentRay; //vecteur directeur du rayon incident
+	sPos orientationVectorRefractedRay;//vecteur directeur du rayon réfracté
+	double teta[2]; //angles (incident et réfracté)
 	sPlanEqua planEqua;
+	sParamEqua refractedRay;
 
 	planEqua.a = face.planEqua.a;
 	planEqua.b = face.planEqua.b;
 	planEqua.c = face.planEqua.c;
 	planEqua.d = face.planEqua.d;
 
+//calcul des coordonnées de I
+	t = (-1)*(incidentRay.x[1]*planEqua.a + incidentRay.y[1]*planEqua.b + incidentRay.z[1]*planEqua.c + planEqua.d) / (incidentRay.x[0]*planEqua.a + incidentRay.y[0]*planEqua.b + incidentRay.z[0]*planEqua.c)
+	pI.x = incidentRay.x[0]*t+incidentRay.x[1];
+	pI.y = incidentRay.y[0]*t+incidentRay.y[1];
+	pI.z = incidentRay.z[0]*t+incidentRay.z[1];
+
+//determination du vecteur normal au plan
 	normalisedVector = normalisedVector(planEqua);
+	orientationVectorIncidentRay.x = incidentRay.x[0];
+	orientationVectorIncidentRay.y = incidentRay.y[0];
+	orientationVectorIncidentRay.z = incidentRay.z[0];
+//calcule des angles incident et réfracté
+	teta = calcAngleWithSnellDescartes(teta, orientationVectorIncidentRay, normalisedVector, refractiveIndexA, refractiveIndexB);
+//determination du vecteur directeur du rayon réfracté
+	orientationVectorRefractedRay.x = (refractiveIndexA/refractiveIndexB) * orientationVectorIncidentRay.x + ((refractiveIndexA/refractiveIndexB)*cos(teta[0]) - cos(teta[1]))*normalisedVector.x;
+	orientationVectorRefractedRay.y = (refractiveIndexA/refractiveIndexB) * orientationVectorIncidentRay.y + ((refractiveIndexA/refractiveIndexB)*cos(teta[0]) - cos(teta[1]))*normalisedVector.y;
+	orientationVectorRefractedRay.z = (refractiveIndexA/refractiveIndexB) * orientationVectorIncidentRay.z + ((refractiveIndexA/refractiveIndexB)*cos(teta[0]) - cos(teta[1]))*normalisedVector.z;
+//équation paramétrique du rayon réfracté
+	refractedRay.x[0] = orientationVectorIncidentRay.x;
+	refractedRay.x[1] = pI.x;
+	refractedRay.x[0] = orientationVectorIncidentRay.y;
+	refractedRay.x[1] = pI.y;
+	refractedRay.x[0] = orientationVectorIncidentRay.z;
+	refractedRay.x[1] = pI.z;
+
+	return refractedRay;
 }
