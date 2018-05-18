75643e3b963c24a09f87539fe2b194578bea31ad
Paul Delarue <Paul Delarue>
2018-05-18 10:40:29 +0200

    fonctions de sphère terminées

--- module c/ellipse.c
+++ module c/ellipse.c
@@ -5,8 +5,66 @@
 
 #define PI 3,1415926535
 
-
+//equation d'une ellipse (x-a)²/alpha²  +  (y-b)²/beta²   +  (z-c)²/gamma²  =  1    alpha, beta et gamma donnent les longueurs des rayons dans les 3 directions,  (a,b,c) sont les coordonnées du centre de l'ellipse
 typedef struct sEllipse_ sEllipse;
 struct sEllipse_{
+	double a;
+	double b;
+	double c;
 
+	double alpha;
+	double beta;
+	double gamma;
 };
+
+
+void* doesCollideEllipse (sEllipse ellipse, sParamEqua lightRay){
+	double F, G, H, delta;
+	double t = 0;
+
+	F = pow(ellipse.beta*ellipse.gamma*lightRay.x[0],2) + pow(ellipse.gamma*ellipse.alpha*lightRay.y[0],2) + pow(ellipse.alpha*ellipse.beta*lightRay.z[0],2);
+	G = 2*(pow(ellipse.beta*ellipse.gamma,2)*lightRay.x[0]*(lightRay.x[1] - ellipse.a) + pow(ellipse.alpha*ellipse.gamma,2)*lightRay.y[0]*(lightRay.y[1] - ellipse.b) + pow(ellipse.beta*ellipse.alpha,2)lightRay.z[0]*(lightRay.z[1] - ellipse.c));
+	H = pow(ellipse.beta*ellipse.gamma,2)*((ellipse.a - 2*lightRay.x[1])*ellipse.a+pow(lightRay.x[1],2)) + pow(ellipse.alpha*ellipse.gamma,2)*((ellipse.b - 2*lightRay.y[1])*ellipse.b+pow(lightRay.y[1],2)) + pow(ellipse.beta*ellipse.alpha,2)*((ellipse.c - 2*lightRay.z[1])*ellipse.c+pow(lightRay.z[1],2)) - pow(ellipse.gamma*ellipse.beta*ellipse.alpha,2);
+//résolution de polynôme de second degré
+	delta = pow(G,2) - 4*F*H;
+
+	if(delta > 0){
+		double t1 = 0, t2 = 0;
+
+		sPos* intersectionPoint = NULL;
+		intersectionPoint = (sPos*) malloc(sizeof(sPos));
+
+		t1 = (-G - sqrt(delta))/2*F;
+		t2 = (-G + sqrt(delta))/2*F;
+
+		if(t1 >= t2){
+			t = t2;
+		}
+		else{
+			t = t1;
+		}
+
+		intersectionPoint->x = lightRay.x[0]*t + lightRay.x[1];
+		intersectionPoint->y = lightRay.y[0]*t + lightRay.y[1];
+		intersectionPoint->z = lightRay.z[0]*t + lightRay.z[1];
+
+		return intersectionPoint;
+
+	}
+	else if(delta = 0){
+		sPos* intersectionPoint = NULL;
+		intersectionPoint = (sPos*) malloc(sizeof(sPos));
+
+		t = -G / 2*F;
+
+		intersectionPoint->x = lightRay.x[0]*t + lightRay.x[1];
+		intersectionPoint->y = lightRay.y[0]*t + lightRay.y[1];
+		intersectionPoint->z = lightRay.z[0]*t + lightRay.z[1];
+
+		return intersectionPoint;
+
+	}
+	else{
+		return false;
+	}
+}
--- module c/sphere.c
+++ module c/sphere.c
@@ -5,7 +5,7 @@
 
 #define PI 3,1415926535
 
-//equation d'une sphère : (x - X)² + (y - Y)² + (z - Z)² = r² avec x,y,z inconnues et X,Y,Z(position du centre de la sphere) et r(rayon) connus
+//equation d'une sphère : (x - x0)² + (y - y0)² + (z - z0)² = r² avec x,y,z inconnues et x0,y0,z0(position du centre de la sphere) et r(rayon) connus
 typedef struct sSphere_ sSphere;
 struct sSPhere{
 	sPos center;
@@ -18,9 +18,10 @@ struct sSPhere{
 void* doesCollideSphere (sSphere sphere, sParamEqua lightRay){
 	double alpha, beta, gamma, delta;
 	double t = 0;
-//equation de la sphere devient (A²+B²+C²)t² + 2(A(a-X) + B(b-Y) + C(c-Z))t + (X-2a)X + (Y-2b)Y + (Z-2c)Z + a² + b² + c² - r² = 0
+//equation de la sphere devient (A²+B²+C²)t² + 2(A(a-x0) + B(b-y0) + C(c-z0))t + (x0-2a)x0 + (y0-2b)y0 + (z0-2c)z0 + a² + b² + c² - r² = 0
 //								  alpha					beta								gamma
-	alpha = lightRay.x[0] + lightRay.y[0] + lightRay.z[0];
+//reverifier les calculs
+	alpha = pow(lightRay.x[0],2) + pow(lightRay.y[0],2) + pow(lightRay.z[0],2);
 	beta = 2*(lightRay.x[0]*(lightRay.x[1] - sphere.center.x) + lightRay.y[0]*(lightRay.y[1] - sphere.center.y) + lightRay.z[0]*(lightRay.z[1] - sphere.center.z));
 	gamma = (sphere.center.x - 2*lightRay.x[1])*lightRay.x[0] + (sphere.center.y - 2*lightRay.y[1])*lightRay.y[0] + (sphere.center.z - 2*lightRay.z[1])*lightRay.z[0] + pow(lightRay.x[1],2) + pow(lightRay.y[1],2) + pow(lightRay.z[1],2) - pow(sphere.r,2);
 //résolution de polynôme de second degré
@@ -67,4 +68,19 @@ void* doesCollideSphere (sSphere sphere, sParamEqua lightRay){
 	}
 }
 
+sPlanEqua makeTangentPlanFromSphere(sPos collisionPoint, sPos centerOfSphere){
+	sPos radiusVector;
+	sPlanEqua tangentPlan;
 
+	radiusVector.x = collisionPoint.x - centerOfSphere.x;
+	radiusVector.y = collisionPoint.y - centerOfSphere.y;
+	radiusVector.z = collisionPoint.z - centerOfSphere.z;
+
+	tangentPlan.a = radiusVector.x;
+	tangentPlan.b = radiusVector.y;
+	tangentPlan.c = radiusVector.z;
+
+	tangentPlan.d = (tangentPlan.a*collisionPoint.x + tangentPlan.b*collisionPoint.y + tangentPlan.c*collisionPoint.z) * (-1);
+
+	return tangentPlan;
+}
