cf991cc56714fbeeb66b0e699cb37ebd7c41f13c
VictorJungISEN <33875047+VictorJungISEN@users.noreply.github.com>
2018-05-23 18:06:23 +0200

    Add files via upload

---
+++ module c/C/C/Trash7/structure.h
@@ -0,0 +1,95 @@
+#pragma once
+#include "bmp.h"
+#include <stdio.h>
+
+typedef struct sFormula_ sFormula;
+struct sFormula_ {
+	int nbX;
+	double *x;
+	int nbY;
+	double *y;
+	int nbZ;
+	double *z;
+};
+
+typedef struct sParametricEquation_ sParamEqua;
+struct sParametricEquation_ {
+	double x[2];
+	double y[2];
+	double z[2];
+};
+
+typedef struct sPosition_ sPos;
+struct sPosition_ {
+	double x;
+	double y;
+	double z;
+};
+
+typedef struct sPlanEquation_ sPlanEqua;
+struct sPlanEquation_ {
+	double a;
+	double b;
+	double c;
+	double d;
+};
+
+typedef struct sFace_ sFace;
+struct sFace_ {
+	int nbPeaks;
+	sPos *peak;
+	sPlanEqua planEqua;
+};
+
+typedef struct sObject_ sObject;
+struct sObject_ {
+	sFormula formula;
+	int nbFaces;
+	sFace *face;
+	sColor color;
+};
+
+typedef struct sImage_ sImage;
+struct sImage_ {
+	char *name;
+	double width;
+	double height;
+	sColor background;
+};
+
+typedef struct sLight_ sLight;
+struct sLight_ {
+	float lightFactor;
+	sParamEqua paramEqua;
+};
+
+typedef struct sSphere_ sSphere;
+struct sSphere_ {
+	sPos center;
+	double radius;
+	sColor color;
+};
+
+typedef struct sParam_ sParam;
+struct sParam_ {
+	sImage image;
+
+	sPos viewerPos;
+
+	int nbObjects;
+	sObject *object;
+
+	int nbSpheres;
+	sSphere *sphere;
+
+	sPos lightSource;
+	sLight light;
+};
+
+int nbLine(FILE *f);
+
+void showStruct(sParam param);
+
+int loadFromFile(sParam *param);
+
+void freeAll(sParam *param);
